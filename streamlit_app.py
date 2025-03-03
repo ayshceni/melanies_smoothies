a5d2ac061f2173a0012d49420a4e572450efd469
ayshceni <ayush.shah@cenitex.vic.gov.au>
2025-03-04 10:59:05 +1100

    Update streamlit_app.py
    adjusting code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 helpful_links = [
@@ -30,6 +31,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     #st.write(ingredients_string)
 
 
@@ -46,7 +50,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+ name_on_order+'!', icon="✅")
 
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
